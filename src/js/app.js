4952d4156f6e1d97da1b80f8fefe515f51d6c06c
JDigar <jdigar96@gmail.com>
2022-05-13 11:36:29 +0000

    empezando

--- src/js/app.js
+++ src/js/app.js
@@ -27,16 +27,17 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.includeCover == false)
+    cover = `<div class="cover"><img src="${variables.background2}" /></div>`;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}</h3>
+          <ul class="position-left">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -55,20 +56,22 @@ window.onload = function() {
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background2:
+      "https://fotografias.lasexta.com/clipping/cmsimages02/2022/03/17/8817DB4C-0E8F-47FF-A87F-DF95C9FDC014/grace-bay-beach_97.jpg?crop=1253,705,x0,y67&width=1600&height=900&optimize=high&format=webply",
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
+    twitter: "@JonnyD",
     github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    linkedin: "JonnyD",
+    instagram: "@Jonydiazg",
+    name: "Padro",
+    lastname: "García",
+    role: "Web Developer",
+    country: "España",
+    city: "Valencia"
   };
   render(window.variables); //render the card for the first time
 
