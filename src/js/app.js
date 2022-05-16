d709ad24c12185d3be0b5d04c749fc561c5842a2
JDigar <jdigar96@gmail.com>
2022-05-16 17:31:10 +0000

    finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -36,9 +36,9 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
-          <h3>${variables.city}</h3>
-          <h3>${variables.country}</h3>
-          <ul class="position-left">
+          <h3>${variables.city}, ${variables.country}</h3>
+        
+          <ul class="${variables.socialMediaPosition}">
             <li><a target="_blank" href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a target="_blank" href="${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a target="_blank" href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
@@ -68,7 +68,7 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: "Padro",
+    name: "Pedro",
     lastname: "García",
     role: "Web Developer",
     country: "España",
